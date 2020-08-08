08ed90e3e3206e3c5b5dc46d1581cfcdb97be72e
WebKawsar <web.kawsarahmed@gmail.com>
2020-08-08 15:06:02 +0600

    some true false value practice

---
+++ true-false.js
@@ -0,0 +1,27 @@
+// Truthy Value 
+// " "
+// 5
+// "0"
+// "undefined"
+// [] {}
+// "false"
+
+// Falsy Value
+// ""
+// 0
+// undefined
+// null
+// false
+// NaN
+
+const age = "undefined";
+
+if(age){
+
+    console.log("Condition is True.");
+
+}
+else {
+    console.log("Condition is False.");
+}
+
