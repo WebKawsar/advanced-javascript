bf82c0f39d3a4f670c36c50bbb376b6969eef71d
WebKawsar <web.kawsarahmed@gmail.com>
2020-08-08 15:57:28 +0600

    null vs undefined concept clear

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+
+// How to get undefined value
+let pakhi;
+// console.log(pakhi);
+
+
+function myFunc(num1, num2){
+    console.log(num1 + num2);
+}
+
+// const reslut = myFunc(5, 36);
+// console.log(reslut);
+
+function para(num1, num2){
+    console.log(num1 + num2);
+}
+
+// const getParam = para(3);
+// console.log(getParam);
+
+
+const premika = {name:"Smart Girl", phone:103606};
+// console.log(premika.bf);
+
+
